1ffcbde4c0053b6988ee74d31296b895a1f5c3e0
Finbar <finbar@tutanota.com>
2022-04-02 20:35:29 +0100

    Formatting

--- src/algorithm_selection.py
+++ src/algorithm_selection.py
@@ -1,20 +1,20 @@
 import os
 import argparse
-import re
 import modelling as exp
 import numpy as np
 import matplotlib.pyplot as plt
+
 plt.style.use('seaborn-whitegrid')
 plt.rc('lines', linewidth=2)
+
 
 def print_data_row(row):
     s = str(row[0]) + ' ' + str(row[1]) + " " + str(row[3]) + " "
     s += exp.scatter_algorithms[int(row[2] - 2)][1]
     return s.strip()
 
-def selection_experiments(args):
 
-    print(args)
+def selection_experiments(args):
 
     X = []
     Y = []
@@ -32,18 +32,18 @@ def selection_experiments(args):
     lb_isend = exp.lin_reg(X, Y)
     vformat = ['\\num{' + "{:.1e}".format(v) + '}' for v in lb_isend]
     el = ", ".join(vformat)
-#    print("lb_isend: ");
-#    print(lb_isend)
-    #train_data_path = 'data/csi_long_'+ str(args.nump)
-    train_data_path = 'data/sonic_long_'+ str(args.nump)
-    #converts data to list
+    #    print("lb_isend: ");
+    #    print(lb_isend)
+    # train_data_path = 'data/csi_long_'+ str(args.nump)
+    train_data_path = 'data/sonic_long_' + str(args.nump)
+    # converts data to list
     train_data_set = exp.exp_data_list(train_data_path)
 
     # Hockney model parameters are measured using collective algorithms
     hockney_model_parameters = []
     coll_algorithms = exp.scatter_algorithms
-    if (args.ver == "2.1"):
-        #Open MPI 2.1 does not contain Linear Non Blocking Algorithm
+    if args.ver == "2.1":
+        # Open MPI 2.1 does not contain Linear Non Blocking Algorithm
         coll_algorithms.pop()
 
     # Calculate latency and bandwidth using collective algorithms, iterate through algorithms
@@ -59,7 +59,7 @@ def selection_experiments(args):
         vformat = ['\\num{' + "{:.1e}".format(v) + '}' for v in value]
         el = ", ".join(vformat)
 
-    #unseen_data_path = 'data/csi_short_' + str(args.nump)
+    # unseen_data_path = 'data/csi_short_' + str(args.nump)
     unseen_data_path = 'data/sonic_short_' + str(args.nump)
     if not unseen_data_path:
         print("Given file is not found!")
@@ -79,25 +79,25 @@ def selection_experiments(args):
     print('----------------------------------------------------------------')
 
     model_opt_alg = exp.optimal_scatter_algorithm_by_model(
-            hockney_model_parameters, unseen_data_set, len(coll_algorithms))
+        hockney_model_parameters, unseen_data_set, len(coll_algorithms))
 
     for analy_est, best_alg in zip(model_opt_alg, best_perf_alg):
         print(print_data_row(analy_est), ' -- ',
-              '{}%'.format(round(analy_est[3]/best_alg[3] * 100)))
+              '{}%'.format(round(analy_est[3] / best_alg[3] * 100)))
 
     print('----------------------------------------------------------------')
 
-    #Newer version of OMPI has a refined algorithm selection process
+    # Newer version of OMPI has a refined algorithm selection process
 
-    if (args.ver == "4.1"):
+    if args.ver == "4.1":
         ompi_opt_alg = exp.new_ompi_optimal_scatter_alg(unseen_data_set)
 
-    elif (args.ver == "2.1"):
+    elif args.ver == "2.1":
         ompi_opt_alg = exp.ompi_optimal_scatter_alg(unseen_data_set)
 
     for ompi_alg, best_alg in zip(ompi_opt_alg, best_perf_alg):
         print(print_data_row(ompi_alg), ' -- ',
-              '{}%'.format(round(ompi_alg[3]/best_alg[3] * 100)))
+              '{}%'.format(round(ompi_alg[3] / best_alg[3] * 100)))
 
     Y_exp = []
     Y_model = []
@@ -105,7 +105,7 @@ def selection_experiments(args):
 
     for alg1, alg2, alg3 in zip(best_perf_alg, model_opt_alg, ompi_opt_alg):
         Y_exp.append(alg1[3])
-        Y_model.append(alg1[3])
+        Y_model.append(alg2[3])
         Y_ompi.append(alg3[3])
     data_types = ['Best',
                   'Model-based',
@@ -134,7 +134,7 @@ def selection_experiments(args):
     i = 0
 
     title_font = {'family': 'monospace',
-                  'color':  'black',
+                  'color': 'black',
                   'weight': 'normal',
                   }
 
@@ -149,7 +149,7 @@ def selection_experiments(args):
         legend = plt.legend(
             loc='upper left',
             frameon=False,
-            title= 'P = ' + str(args.nump),
+            title='P = ' + str(args.nump),
             prop=dict(size='small'))
         i += 1
 
@@ -160,13 +160,11 @@ def selection_experiments(args):
     plt.title(f'{coll_name} P = {args.nump}', fontdict=title_font)
     plt.show()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--nump", required=True, help="Number of Processes")
     parser.add_argument("--ver", required=True, help="Open MPI Version, Supported: 2.1, 4.1")
     args = parser.parse_args()
     args.path = os.getcwd()
     selection_experiments(args)
-
-
